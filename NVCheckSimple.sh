146e6f827e983013ddee2fd4d40833f299fc22ba
Sgtsmi1es <37192620+Sgtsmi1es@users.noreply.github.com>
2020-05-10 03:16:11 -0500

    Name Change
    Changed name, added changes from develop /NVCheck/commit/d3587bed978ec6f64fb9b6d1c1b923ab92257fca to simple

---
+++ NVCheckSimple.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This script was created to fix the plex docker not launching when backups or restarts are run
+echo "************ "$(date -u)" NVCheck Simple ************"  | tee -a ~/logs/NVCheck.log
+echo $(date -u) "Checking if Plex Docker is running..."  | tee -a ~/logs/NVCheck.log
+if docker inspect -f '{{.State.Running}}' plex | grep -q 'true'; # This loop checks the running status of the plex docker and then re-runs the nvidia tag if it does not find plex running.
+    then
+        echo $(date -u) "plex already started, script finished." | tee -a ~/logs/NVCheck.log
+    else
+        echo $(date -u) "plex not running, running nvidia ansible tag" | tee -a ~/logs/NVCheck.log
+        cd ~/cloudbox && sudo ansible-playbook cloudbox.yml --tags nvidia
+        docker start plex
+        echo $(date -u) "nvidia tag run, plex docker start command sent, checking if plex is running..." | tee -a ~/logs/NVCheck.log
+        if docker inspect -f '{{.State.Running}}' plex | grep -q 'true'; #This checks to see if the fix worked and outputs the result.
+            then
+                echo $(date -u) "plex started after the rerun of nvidia Tag, script finished." | tee -a ~/logs/NVCheck.log
+            else
+                echo $(date -u) "WARNING: Plex still not running after re-runniong Nvidia tag, check system for faults." | tee -a ~/logs/NVCheck.log
+        fi
+fi
